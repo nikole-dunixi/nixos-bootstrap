d0b3b1e92dae70d9e361e62f7ef81a5f97d5e2bf
Nikole Dunixi <1402178+nikole-dunixi@users.noreply.github.com>
2026-05-04 01:01:54 -0600

    chore: remove noise

--- flake.nix
+++ flake.nix
@@ -54,7 +54,5 @@
         ];
       };
     };
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
   };
 }
